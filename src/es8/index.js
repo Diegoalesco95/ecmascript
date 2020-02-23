d1eaed4f76f985bcf452666ae7aa996e2341776d
Diego Alejandro Osorio <42728227+Diegoalesco95@users.noreply.github.com>
2020-02-23 16:25:19 -0500

    Async Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -32,3 +32,33 @@ console.log(string.padEnd(12, "-----"));
 const Obj = {
   name: "oscar" //,
 };
+
+// Asyc Await ------------------------------------------------
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve("Hello, World!"), 3000)
+      : reject(new Error("Error"));
+  });
+};
+
+// sc8
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+};
+
+helloAsync();
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+anotherFunction();
